3cbba99c3c21638497dadf6a89a20bb261b510e1
Igor <fullstack@naladka.su>
2023-03-22 21:01:45 +0300

    add code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+	for (int j = i; j < n; j++)
+	{
+		matrix[i, j] = (i + 1) * (j + 1);
+		matrix[j, i] = (i + 1) * (j + 1);
+	}
+	Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+	for (int j = 0; j < n; j++)
+	{
+		Console.Write(matrix[i, j]);
+		Console.Write("\t");
+	}
+	Console.WriteLine();
+}
